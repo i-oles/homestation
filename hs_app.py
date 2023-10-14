f40f0e4cc980d637456babf973435a0fbba06bd4
Igor Oles <igor.oles@websensa.com>
2023-10-14 15:16:42 +0200

    deploy try

--- hs_app.py
+++ hs_app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, json, Response
 from tinydb import TinyDB
 
 from internal.domain import domain
-from config import config
+from config.config import DB_FILE_NAME
 from internal.repo.tiny_db.tiny_db import TinyDbRepo
 from internal.service.light.light import Light
 import os
@@ -12,7 +12,8 @@ import os
 homestation_app = Flask(__name__)
 
 absolute_path = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(absolute_path, config.DB_FILE_NAME)
+db_path = os.path.join(absolute_path, DB_FILE_NAME)
+print(db_path)
 db = TinyDB(db_path)
 
 repo = TinyDbRepo(db)
